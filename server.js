d12a354b5327bcfc84cf1f0d25a2519463502090
Adam Buggia <abuggia@gmail.com>
2011-08-27 12:35:31 -0700

    Refactoring, using html5 boilerplate and express for node.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 
-var fs = require('fs'),
-    http = require('http');
+var express = require('express'),
+    app = express.createServer();
 
-server = http.createServer(function(req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.write(data);
-    res.end();
-  });
-})
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
 
-var port = process.env.PORT || 8000
-server.listen(port);
+var port = process.env.PORT || 3000
+app.listen(port)
 
-var everyone = require("now").initialize(server);
+var everyone = require("now").initialize(app);
 everyone.now.distributeMessage = function(msg) {
   everyone.now.receiveMessage(this.now.name, msg);
 }
