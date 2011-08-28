9fb993c2c45fa0b75e1585a67dfba765b2db87f2
Adam Buggia <abuggia@gmail.com>
2011-08-28 12:57:07 -0700

    First added Mongo.

--- server.js
+++ server.js
@@ -2,12 +2,43 @@
 var express = require('express'),
     app = express.createServer();
 
+var mongoose = require('mongoose');
+var mongo_uri = process.env.MONGOLAB_URI || 'mongodb://localhost/db'
+
+
+mongoose.model('User', new mongoose.Schema({
+  email: String,
+  handle: String,
+  password: String
+}));
+
+var db = mongoose.connect(mongo_uri);
+
+var User = mongoose.model('User');
+
 app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 var port = process.env.PORT || 3000
 app.listen(port)
+
+app.get('/user/:id', function(req, res) {
+  User.findOne({ email: req.params.email }, function(err, result) {
+    if (err) {
+      res.send(404);
+    } else {
+      res.contentType('json');
+      res.send({ "handle" : handle });
+     }
+  });
+});
+
+
+/* Now JS */
 
 var everyone = require("now").initialize(app, {"socketio": {"transports": ["xhr-polling"]}});
 everyone.now.distributeMessage = function(msg) {
